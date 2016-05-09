ff599d29c9c52406c905e35b3d9b00b036518489
ParallelR <patric.zhao@gmail.com>
2016-05-09 14:11:27 +0800

    wrapper between R and C/C++
    .Call function

---
+++ ParDNN/cudaR.R
@@ -0,0 +1,37 @@
+# ParallelR:  GTC2016, Unblock Performance limit in R by CUDA
+
+# preload static object file
+dyn.load("cudaR.so")
+
+# GPU version of pmax
+pmax.cuda <- function(A, threshold, devID=0L)
+{
+  rst <- .Call("pmax_cuda",
+                A,
+                threshold,
+                as.integer(devID)
+	      )
+  dim(rst) <- dim(A)
+  return(rst)
+}
+
+# GPU version of GEMM from cuBLAS
+cuBLAS <- function(A, B, transA=FALSE, transB=FALSE, devID=0L) 
+{
+  tA <- ifelse(transA == FALSE, 0L, 1L)
+  tB <- ifelse(transB == FALSE, 0L, 1L)
+  rst <- .Call("gemm_cuda",
+                A,
+		B,
+		tA,
+		tB,
+		as.integer(devID)
+	      )
+  	  
+  row.size <- dim(A)[1]
+  col.size <- dim(B)[2]
+  if(tA == 1) row.size <- dim(A)[2]
+  if(tB == 1) col.size <- dim(B)[1]	  
+  dim(rst) <- c(row.size, col.size)
+  return(rst)	  
+}
