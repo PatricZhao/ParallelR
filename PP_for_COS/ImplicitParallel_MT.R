b614dc6309b499d689b7fe69847d0d1ebdbe900d
PengZhao <patric.zhao@gmail.com>
2016-09-14 11:36:52 +0800

    Wrapper the thread setting

--- PP_for_COS/ImplicitParallel_MT.R
+++ PP_for_COS/ImplicitParallel_MT.R
@@ -2,13 +2,47 @@
 # Author: Peng Zhao, 8/30/2016
 
 # comparison of single thread and multiple threads run
-# using Internal function to set thread numbers, not very grace, but don't find a good way till now
-# suggestions?
+# using Internal function to set thread numbers, not very grace, but don't find a good way till now.
+# Ang suggestion?
+setNumThreads <- function(nums=1) {
+  .Internal(setMaxNumMathThreads(nums));
+  .Internal(setNumMathThreads(nums));
+}
+
+# Testing dist funciton with single (1) and multiple (20) threads
 for(i in 6:11) {
     ORDER <- 2^i
     m <- matrix(rnorm(ORDER*ORDER),ORDER,ORDER);
-    .Internal(setMaxNumMathThreads(1)); .Internal(setNumMathThreads(1)); res <- system.time(d <- dist(m))
-     print(res)
-    .Internal(setMaxNumMathThreads(20)); .Internal(setNumMathThreads(20)); res <- system.time(d <- dist(m))
-     print(res)
+    setNumThreads(1)
+    res <- system.time(d <- dist(m))
+    print(res)
+    setNumThreads(20)
+    res <- system.time(d <- dist(m))
+    print(res)
 }
+
+# Results
+#user  system elapsed 
+#  0.002   0.000   0.002 
+#   user  system elapsed 
+#  0.076   0.001   0.005 
+#   user  system elapsed 
+#  0.012   0.000   0.012 
+#   user  system elapsed 
+#  0.076   0.000   0.005 
+#   user  system elapsed 
+#  0.089   0.000   0.089 
+#   user  system elapsed 
+#  0.164   0.001   0.020 
+#   user  system elapsed 
+#  0.716   0.000   0.717 
+#   user  system elapsed 
+#  0.993   0.001   0.117 
+#   user  system elapsed 
+#  4.651   0.000   4.656 
+#   user  system elapsed 
+#  5.728   0.001   0.564 
+#   user  system elapsed 
+#136.895   0.005 137.030 
+#   user  system elapsed 
+#215.808   0.048  21.490 
