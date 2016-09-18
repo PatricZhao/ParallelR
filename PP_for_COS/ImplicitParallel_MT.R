199cc4826b9a4270634de2573f8d61d3c08e6e8c
PengZhao <patric.zhao@gmail.com>
2016-09-18 10:49:55 +0800

    Update ImplicitParallel_MT.R

--- PP_for_COS/ImplicitParallel_MT.R
+++ PP_for_COS/ImplicitParallel_MT.R
@@ -1,63 +1,6 @@
 # Examples for the R and Parallel Computing blog in COS website （cos.name)
 # Author: Peng Zhao, 8/30/2016
 
-# Example 1: Using Parallel Libraries
-# using 'deepnet' and 'mnist' dataset for example
-# see the performance w/ and w/o parallel BLAS backend
-
-#install.packages("data.table")
-#install.packages("deepnet")
-
-library(data.table)
-library(deepnet)
-
-# V785 is the label
-mnist.train <- as.matrix(fread("./train.csv", header=F))
-mnist.test  <- as.matrix(fread("./test.csv", header=F))
-
-x <- mnist.train[, 1:784]/255
-y <- model.matrix(~as.factor(mnist.train[, 785])-1)
-
-system.time(
-nn <- dbn.dnn.train(x,y,
-                    hidden=c(64),
-                    output="softmax",
-                    batchsize=128, numepochs=100, learningrate = 0.1)
-)
-
-
-
-# OpenBLAS
-# env LD_PRELOAD=/.../tools/OpenBLAS/lib/libopenblas.so R CMD BATCH deepnet_mnist.R
-
-#begin to train dbn ......
-#training layer 1 rbm ...
-#dbn has been trained.
-#begin to train deep nn ......
-####loss on step 10000 is : 0.193343
-####loss on step 20000 is : 0.121218
-####loss on step 30000 is : 0.127029
-####loss on step 40000 is : 0.159519
-#deep nn has been trained.
-#     user    system   elapsed 
-# 2197.394 10496.190   867.748
-
-# native R
-# R CMD BATCH deepnet_mnist.R
-#begin to train dbn ......
-#training layer 1 rbm ...
-#dbn has been trained.
-#begin to train deep nn ......
-####loss on step 10000 is : 0.179346
-####loss on step 20000 is : 0.123266
-####loss on step 30000 is : 0.136734
-####loss on step 40000 is : 0.085222
-#deep nn has been trained.
-#    user   system  elapsed 
-#2110.710    2.311 2115.042
-
-
-
 # Example 2: Using MultiThreading Functions
 # comparison of single thread and multiple threads run
 # using Internal function to set thread numbers, not very grace, but don't find a good way till now.
